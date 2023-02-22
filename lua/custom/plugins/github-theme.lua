fd75e1802eb2e5c9bf09b44dd1588e101b4d3c9d
alinademi <nademi@gmail.com>
2023-02-21 21:34:16 -0800

    add-github theme

--- lua/custom/plugins/github-theme.lua
+++ lua/custom/plugins/github-theme.lua
@@ -1,32 +1,24 @@
 -- including github-nvim-theme.
 -- currently the latest stable tag is v0.0.7
--- https://github.com/projekt0n/github-nvim-theme
+-- https://github.com/projekt0n/github-nvim-theme/releases/tag/v0.0.7
 
 return {
-"projekt0n/github-nvim-theme",
-config = function()
-require("github-theme").setup({
-theme_style = "dark_default",
-comment_style = "italic",
-keyword_style = "NONE",
-function_style = "NONE",
-variable_style = "NONE",
-sidebars = {"qf", "vista_kind", "terminal", "packer"},
-  -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-  colors = {hint = "orange", error = "#ff0000"},
-
-  -- Overwrite the highlight groups
-  overrides = function(c)
-    return {
-      htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
-      DiagnosticHint = {link = "LspDiagnosticsDefaultHint"},
-      -- this will remove the highlight groups
-      TSField = {},
-    }
-  end
-})
-end,
-setup = function()
-require("lazy").loader("github-nvim-theme")
-end
+  'projekt0n/github-nvim-theme',
+  config = function()
+    require('github-theme').setup({
+      theme_style = "dark",
+      function_style = "italic",
+      sidebars = { "qf", "vista_kind", "terminal", "packer" },
+      colors = { hint = "orange", error = "#ff0000" },
+      overrides = function(c)
+        return {
+          htmlTag = { fg = c.red, bg = "#111a24", sp = c.hint, style = "underline" },
+          DiagnosticHint = { link = "LspDiagnosticsDefaultHint" },
+          TSField = {},
+        }
+      end
+    })
+  end,
+  -- Set the priority value to a higher value to ensure that the colorscheme is loaded before other plugins
+  priority = 1000,
 }
