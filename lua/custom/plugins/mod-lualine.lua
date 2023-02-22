f123415a4e1d7ba0429e26e22bb647dda09d20d8
alinademi <nademi@gmail.com>
2023-02-21 22:07:40 -0800

    fix-lualine config syntax issue

--- lua/custom/plugins/mod-lualine.lua
+++ lua/custom/plugins/mod-lualine.lua
@@ -1,16 +1,14 @@
-require('lualine').setup({
-   options = {
-      icons_enabled = true,
-      theme = 'iceberg_dark',
-      component_separators = { left = '|', right = '|' },
-      section_separators = '',
-   },
-   sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = { 'filename' },
-      lualine_x = { 'filetype' },
-      lualine_y = {},
-      lualine_z = { 'location' }
+return {
+   { -- Set lualine as statusline
+      'nvim-lualine/lualine.nvim',
+      -- See `:help lualine.txt`
+      opts = {
+         options = {
+            icons_enabled = false,
+            theme = 'iceberg_dark',
+            component_separators = '|',
+            section_separators = '',
+         },
+      },
    }
-})
+}
