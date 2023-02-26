b7a768422edce385221e82c6b098929c1c12a58a
alinademi <nademi@gmail.com>
2023-02-26 11:47:00 -0800

    add/ better netrw experience
    add/ ctrl+n for revealing neo tree

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,2 +1,13 @@
 -- we are using this file to modify the default settings of plugins
 -- This is a good place to put our own custom keybindings
+
+
+
+-- Better Netrw
+vim.g.netrw_banner = 0 -- Hide banner
+vim.g.netrw_browse_split = 4 -- Open in previous window
+vim.g.netrw_alth = 1 -- Open with right splitting
+vim.g.netrw_liststyle = 3 -- Tree-style view
+vim.g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]] -- use .gitignore
+
+vim.api.nvim_set_keymap("n", "<C-n>", ":NeoTreeReveal<CR>", { noremap = true, silent = true })
