550549bf5129ab485195943ea6f8e28470f145a2
alinademi <nademi@gmail.com>
2023-02-27 15:08:04 -0800

    update/ better native github feeling with some color change

--- lua/custom/plugins/github-theme.lua
+++ lua/custom/plugins/github-theme.lua
@@ -7,11 +7,27 @@ return {
   config = function()
     require('github-theme').setup({
       theme_style = "dark_default",
+      -- the background color of the whole editor
       function_style = "italic",
       sidebars = { "qf", "vista_kind", "terminal", "packer" },
       colors = {
+        bg = "#111a24",
         hint = "orange",
         error = "#ff0000",
+        syntax = {
+          comment = '#5b6aa0',
+          constant = '#79c0ff',
+          string = '#a5d6ff',
+          variable = '#C9D1D9',
+          keyword = '#ED8B84',
+          func = '#d2a8ff',
+          func_param = '#C9D1D9',
+          match_paren_bg = '#105357',
+          tag = '#7ee787',
+          html_arg = '#ff7b72',
+          param = '#C9D1D9',
+          json_label = '#79c0ff',
+        },
       },
       overrides = function(c)
         return {
