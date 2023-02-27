906c0e1b161778fcc6d16487f98d734bce9081ed
alinademi <nademi@gmail.com>
2023-02-26 16:42:38 -0800

    update/ disable neotree for now

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,15 +1,25 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  enabled = false,
   version = "*",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
+  config = function()
     -- Unless you are still migrating, remove the deprecated commands from v1.x
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      git_status = {
+        window = {
+          position = "float",
+          width = 0.3,
+          height = 0.3,
+          border = "rounded",
+        },
+      }
+    }
   end,
 }
