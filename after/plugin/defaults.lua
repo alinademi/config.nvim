3fead290c1a5e4659539236115272680d5512669
alinademi <nademi@gmail.com>
2023-02-27 17:19:05 -0800

    add/ better netrw experience

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -8,6 +8,13 @@ require "custom.keymaps.keymaps"
 ------------------------------
 vim.g.netrw_banner = 0 -- Hide banner
 vim.g.netrw_browse_split = 4 -- Open in previous window
-vim.g.netrw_alth = 1 -- Open with right splitting
 vim.g.netrw_liststyle = 3 -- Tree-style view
+vim.g.netrw_altv = 1 -- Open with right splitting
+vim.g.netrw_winsize = 20 -- Set window width
+vim.cmd [[
+augroup ProjectDrawer
+  autocmd!
+  autocmd VimEnter * :Vexplore
+augroup END
+]] -- Open drawer on startup
 vim.g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]] -- use .gitignore
