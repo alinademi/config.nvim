ac9d1b3728c4cbaefc0dbbc02c55c3e6701add06
alinademi <nademi@gmail.com>
2023-03-01 11:00:13 -0800

    update/ disable netrw show on startup

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -11,10 +11,10 @@ vim.g.netrw_browse_split = 4 -- Open in previous window
 vim.g.netrw_liststyle = 3 -- Tree-style view
 vim.g.netrw_altv = 1 -- Open with right splitting
 vim.g.netrw_winsize = 20 -- Set window width
-vim.cmd [[
-augroup ProjectDrawer
-  autocmd!
-  autocmd VimEnter * :Vexplore
-augroup END
-]] -- Open drawer on startup
+-- vim.cmd [[
+-- augroup ProjectDrawer
+--   autocmd!
+--   autocmd VimEnter * :Vexplore
+-- augroup END
+-- ]] -- Open drawer on startup
 vim.g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]] -- use .gitignore
