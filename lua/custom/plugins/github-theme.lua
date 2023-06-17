9e9399ec5932b18074edc9e80e8bb168fad663f8
alinademi <nademi@gmail.com>
2023-06-16 17:46:23 -0700

    fix/ some deprecations for github theme

--- lua/custom/plugins/github-theme.lua
+++ lua/custom/plugins/github-theme.lua
@@ -25,34 +25,38 @@ return {
           },
         },
       },
-      colors = {
-        bg = "#111a24",
-        hint = "orange",
-        error = "#ff0000",
-        syntax = {
-          comment = '#5b6aa0',
-          constant = '#79c0ff',
-          string = '#a5d6ff',
-          variable = '#C9D1D9',
-          keyword = '#ED8B84',
-          func = '#d2a8ff',
-          func_param = '#C9D1D9',
-          match_paren_bg = '#105357',
-          tag = '#7ee787',
-          html_arg = '#ff7b72',
-          param = '#C9D1D9',
-          json_label = '#79c0ff',
+      palettes = {
+        github_dark = {
+          bg = "#111a24",
+          hint = "orange",
+          error = "#ff0000",
+          syntax = {
+            comment = '#5b6aa0',
+            constant = '#79c0ff',
+            string = '#a5d6ff',
+            variable = '#C9D1D9',
+            keyword = '#ED8B84',
+            func = '#d2a8ff',
+            func_param = '#C9D1D9',
+            match_paren_bg = '#105357',
+            tag = '#7ee787',
+            html_arg = '#ff7b72',
+            param = '#C9D1D9',
+            json_label = '#79c0ff',
+          },
         },
       },
       -- Overwrite the highlight groups
-      overrides = function(c)
-        return {
-          htmlTag = { fg = c.red, bg = '#282c34', sp = c.hint, style = 'underline' },
-          DiagnosticHint = { link = 'LspDiagnosticsDefaultHint' },
-          -- this will remove the highlight groups
-          TSField = {},
-        }
-      end,
+      -- @Todo: overrides is deprecated, use `groups` instead
+      -- https://github.com/projekt0n/github-nvim-theme/discussions/198#discussioncomment-5700014
+      -- overrides = function(c)
+      --   return {
+      --     htmlTag = { fg = c.red, bg = '#282c34', sp = c.hint, style = 'underline' },
+      --     DiagnosticHint = { link = 'LspDiagnosticsDefaultHint' },
+      --     -- this will remove the highlight groups
+      --     TSField = {},
+      --   }
+      -- end,
     })
 
     vim.cmd('colorscheme github_dark')
